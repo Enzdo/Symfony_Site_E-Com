408dbdd2b9e3ebd5b1f78b9bb0e8ede4174753ae
enzdo <enzodeniau2003@gmail.com>
2023-02-17 19:01:37 +0100

    :sparkles: add dashboard admin and crud Users

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,6 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
---
+++ src/Controller/Admin/UsersCrudController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Users;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+
+class UsersCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Users::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInPlural('Utilisateurs')
+            ->setEntityLabelInSingular('Utilisateur')
+
+            ->setPageTitle("index", "SymRecipe - Administration des utilisateurs")
+
+            ->setPaginatorPageSize(10);
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')
+                ->hideOnForm(),
+            TextField::new('lastname'),
+            TextField::new('firstname'),
+            TextField::new('email')
+                ->hideOnIndex(),
+            ArrayField::new('roles'),
+            DateTimeField::new('createAt')
+                ->hideOnForm()
+                ->setFormTypeOption('disabled', 'disabled'),
+        ];
+    }
+}
